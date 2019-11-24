f6996dd6e9c6a8a9ab238abacc910d0d88d67717
Litian Yang <46035489+litianryang@users.noreply.github.com>
2019-11-24 15:26:51 -0500

    Add files via upload

---
+++ DataConversion.py
@@ -0,0 +1,63 @@
+from collections import Counter
+import csv
+import datetime
+
+months = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
+}
+
+tau = 30
+
+booking_tuples = []
+with open('./CSV/H2.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    first_line = True
+    
+    for row in csv_reader:
+        if first_line:
+            first_line = False
+        else:
+            daysPastInt = int(row['LeadTime'])
+            if(daysPastInt < tau):
+                daysPast = datetime.timedelta(daysPastInt)
+                bookingDate = datetime.date(int(row['ArrivalDateYear']), int(months[row['ArrivalDateMonth']]), int(row['ArrivalDateDayOfMonth']))
+                bookDate = bookingDate - daysPast
+                booking_tuples.append((bookDate, daysPastInt))
+                
+booking_tuples = sorted(booking_tuples)
+booking_counted = Counter(booking_tuples)
+booking_by_day = {}
+for key in booking_counted.keys():
+    if key[0] not in booking_by_day:
+        booking_by_day[key[0]] = [0,0,0,0,0,0,0,0,0,0,
+                                  0,0,0,0,0,0,0,0,0,0,
+                                  0,0,0,0,0,0,0,0,0,0] #tau days of 0s
+    booking_by_day[key[0]][key[1]] = booking_counted[key]
+
+with open('./Output/H2Formatted.csv', mode='w') as output_file:
+    fieldnames = ['BookDateYear', 'BookDateMonth', 'BookDateDayOfMonth', 'Bookings', 'Month', 'DOW']
+    writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+    writer.writeheader()
+    
+    oneDay = datetime.timedelta(1)
+    for key in booking_by_day.keys():
+        months = []
+        dow = []
+        bookingDate = key + oneDay
+        for i in range(tau):
+            months.append(bookingDate.month)
+            dow.append(bookingDate.weekday())
+            bookingDate = bookingDate + oneDay
+        writer.writerow({'BookDateYear': key.year, 'BookDateMonth': key.month, 'BookDateDayOfMonth': key.day, 'Bookings': booking_by_day[key], 'Month': months, 'DOW': dow})
+print("Done")
