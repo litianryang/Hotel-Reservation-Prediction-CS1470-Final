4c005e50497512b32b0c2274ee0f32392d460ef9
Diane Mutako <dianemutako@dianes-mbp.devices.brown.edu>
2019-12-15 17:05:54 -0500

    Visualizations and error calculations added

--- Model.py
+++ Model.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import sys
 from Preprocess import get_data
+import matplotlib.pyplot as plt
 
 class Model(tf.keras.Model):
     def __init__(self):
@@ -9,36 +10,9 @@ class Model(tf.keras.Model):
         super(Model, self).__init__()
         #parameters
         self.batch_size = 15
-        self.epoch = 300 #evaluate model after 300 examples
+        self.epoch = 300 
         self.dropout_rate = 0.6
         
-        #Calculations for output sizes for each layer
-        
-        # Month+DOW = 60 out | 30 + 30 = 60
-        # LSTM Time = 86880 | 120 out
-        # Dense Time = 14520 | 120 out | (14520 - 120 out) / 120 out = 120 LSTM in
-        
-        # Past Days Bookings = 210 out | 7 * 30 = 210
-        # LSTM Book = 353640 | 210 out
-        # Dense Book = 44310 | 210 out | (44310 - 210 out) / 210 out = 210 LSTM in
-        
-        # Time-Booking Concatenated = 330 out | 120 + 210 = 330
-        # Dense Output = 9930 | 30 out | (9930 - 30 out) / 30 out = 330 in
-        # Final Output = 30
-        
-        # Possible Book + Time values
-        # 10 = YES, 320 = NO
-        # 20 = YES, 310 = NO
-        # 30 = YES, 300 = NO
-        # 40 = YES, 290 = NO
-        # 50 = NO
-        # 60 = YES, 270 = NO
-        # 70 = NO
-        # 80 = NO
-        # 90 = NO
-        # 100 = NO
-        # 110 = YES, 220 = NO
-        # 120 = YES, 210 = YES!!!! Square roots...
         
         # Output sizes for the time layers
         time_lstm_units = 120
@@ -79,21 +53,15 @@ class Model(tf.keras.Model):
         """
         #time input passing its layers
         time_output, time_state_h, time_state_c = self.lstm_time(inputs=time_input)#, initial_state=time_state)
-        print("TIME LSTM: " + str(time_output.shape))
         time_output = self.dense_time(time_output)
-        print("TIME Dense: " + str(time_output.shape))
 
         #historical booking input passing its layers
         book_output, book_state_h, book_state_c = self.lstm_book(inputs=book_input)#, initial_state=book_state)
-        print("BOOK LSTM: " + str(book_output.shape))
         book_output = self.dense_book(book_output)
-        print("BOOK Dense: " + str(book_output.shape))
 
         #concat the two ouputs and pass the final layer to produce output desired
         tmbk_input = tf.concat([time_output, book_output], axis=1)
-        print("Concat" + str(tmbk_input.shape))
         final_output = self.dense_final(tmbk_input)
-        print("Final Dense: " + str(final_output.shape))
 
         return final_output, [time_state_h, time_state_c], [book_state_h, book_state_c]
 
@@ -113,7 +81,6 @@ def train(model, data):
     book_state = None
     
     batch_len = model.batch_size
-    #i=0
     for i in range(len(todayBookings) // batch_len):
         batch_labels = tf.convert_to_tensor(todayBookings[i * batch_len : (i+1) * batch_len], dtype=tf.float32)
         batch_dates = tf.convert_to_tensor(futureDates[i * batch_len : (i+1) * batch_len], dtype=tf.float32)
@@ -121,15 +88,12 @@ def train(model, data):
         with tf.GradientTape() as tape:
             probs, time_state, book_state = model(batch_dates, batch_bookings, time_state, book_state)
             loss = model.loss(probs, batch_labels)
-            print("Loss round " + str(i) + ": " + str(loss))
         
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     
     np.set_printoptions(precision=3)
     np.set_printoptions(suppress=True)
-    print(probs)
-    print(batch_labels)
 
 def test(model, data):
     todayBookings, futureDates, pastBookings = data
@@ -137,16 +101,46 @@ def test(model, data):
     loss = 0    
     batch_len = model.batch_size
     num_of_batchs = len(todayBookings) // batch_len
-    print("NUM OF BATCHES: " + str(num_of_batchs))
+
+    preds, labels = [],[]
     for i in range(num_of_batchs):
         batch_labels = tf.convert_to_tensor(todayBookings[i * batch_len : (i+1) * batch_len], dtype=tf.float32)
         batch_dates = tf.convert_to_tensor(futureDates[i * batch_len : (i+1) * batch_len], dtype=tf.float32)
         batch_bookings = tf.convert_to_tensor(pastBookings[i * batch_len : (i+1) * batch_len], dtype=tf.float32)
         with tf.GradientTape() as tape:
             probs, time_state, book_state = model(batch_dates, batch_bookings, None, None)
+            for i in range (batch_len):
+                preds.append(probs[i])
+                labels.append(batch_labels[i])
             loss += model.loss(probs, batch_labels)
+    return np.exp(loss / num_of_batchs), tf.convert_to_tensor(labels), tf.convert_to_tensor(preds)
+
+def compute_losses (labels, predictions):
+    """ uses labels and predictions to calculate all the errors used as measures of accuracy
+    """
+    mse = tf.losses.mean_squared_error(predictions, labels)
+    rmse = tf.sqrt(mse)
+    mae = tf.losses.mae(predictions, labels)
+    return mae, rmse, mse
+def visualize(labels, predictions):
+    """ labels: label for the nex thing
+        prediction: predicted bookings"""
+
+    labels, predictions = tf.transpose(labels), tf.transpose(predictions)
+
+    labels, predictions = tf.reduce_mean(labels, axis=1), tf.reduce_mean(predictions, axis=1)
+    labels, predictions, days = np.array(labels), np.array(predictions), np.arange(0, 30)
+    mae, rmse, mse = compute_losses(labels, predictions)
+    print('Losses: '"MAE:,", mae, '\n',  "RMSE:,", rmse, '\n', " MSE: ", mse)
     
-    return np.exp(loss / num_of_batchs)
+    #figures
+    plt.plot(days, predictions, label='Predictions', color='blue', linewidth=1)
+    plt.plot(days, labels, label='Observed', color='red', linewidth=1)
+    plt.ylabel("Number of predictions")
+    plt.xlabel("Day of the month")
+    plt.title("Observed predicted bookings vs observed bookings", color="black")
+    plt.legend()
+    plt.show()
 
 def main ():
     #specify user input to include a hotel type: if CITY then run model on H1 data; else run model on H2 data
@@ -163,10 +157,11 @@ def main ():
     
     #initialize model
     model = Model()
-    #for i in range(10):
-    train(model, train_data)
+    for i in range(15):
+        train(model, train_data)
+    perp, labels, preds = test(model, test_data)
+    visualize(labels, preds)
     
-    print("Perplexity: " + str(test(model, test_data)))
 
 if __name__ == '__main__':
    main()
